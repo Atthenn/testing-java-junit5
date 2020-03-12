946fbcf8686f7de36473f8137c01622c1e21ecc9
atena kalaki <atena.kalaki@gmail.com>
2020-03-12 16:03:24 +1000

    Assertion for All

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    @Test
+    void groupedAssertion() {
+
+        Person person = new Person(1L,"atena","Kalaki");
+
+        assertAll(
+                "test props set",
+                () -> assertEquals("atena", person.getFirstName()),
+                () -> assertEquals("Kalaki", person.getLastName())
+        );
+    }
+    @Test
+    void groupedAssertionMsg() {
+
+        Person person = new Person(1L,"atena","Kalaki");
+
+        assertAll(
+                "test props set2",
+                () -> assertEquals("atena", person.getFirstName(),"wrong first name"),
+                () -> assertEquals("Kalaki", person.getLastName(),"wrong last name")
+        );
+
+    }
+}
