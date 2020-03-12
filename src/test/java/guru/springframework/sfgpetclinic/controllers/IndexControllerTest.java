869ab527bb7cf03e27a8412f0169aad998f170ae
atena kalaki <atena.kalaki@gmail.com>
2020-03-12 13:46:48 +1000

    Assertion

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+
+    @Test
+    void index() {
+        Assertions.assertEquals("index", controller.index());
+       // Assertions.assertEquals("indexd", controller.index(), "it is not matched");
+
+    }
+
+    @Test
+    void oupsHandler() {
+        Assertions.assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "dddddd");
+    }
+}
