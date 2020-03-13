2cd120782debd783328bf8962eb72acb8c6dbf3d
atena kalaki <atena.kalaki@gmail.com>
2020-03-13 17:14:58 +1000

    testing Hamcrest

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OwnerTest {
@@ -23,7 +25,7 @@ class OwnerTest {
 
         );
 
-
+        assertThat(owner.getCity(),is("Paris"));
 
     }
 }
